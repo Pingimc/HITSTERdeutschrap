5db21f8c727b20e17ac184fa2e796b7fdb5cc2f0
Pingimc <tillklebeck@yahoo.com>
2026-01-01 18:15:34 +0100

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,5 +1,5 @@
 const SONGS = {
-  "HITSDA-01": { youtubeId: "VIDEO_ID_1", title: "Song 1", artist: "Artist 1" },
+  "HITSDA-01": { youtubeId: "Qdxnl6f2qr4", title: "Song 1", artist: "Artist 1" },
   "HITSDA-02": { youtubeId: "VIDEO_ID_2", title: "Song 2", artist: "Artist 2" },
   "HITSDA-03": { youtubeId: "VIDEO_ID_3", title: "Song 3", artist: "Artist 3" },
 };
